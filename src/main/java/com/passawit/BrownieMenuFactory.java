671cb6330dfc98c2ed05e7f0760b6753927883ec
dekdew <passawit1999@gmail.com>
2019-09-04 01:45:57 +0700

    🛒 add cart and checkout

--- src/main/java/com/passawit/BrownieMenuFactory.java
+++ src/main/java/com/passawit/BrownieMenuFactory.java
@@ -2,22 +2,22 @@ package com.passawit;
 
 public class BrownieMenuFactory {
   public static final BrownieMenu[] BROWNIE_MENUS = new BrownieMenu[] {
-    new BrownieMenu("Set A", "A swirl brownies set include Tahini swirl, Condensed milk and Jam", new Brownie[] {
+    new BrownieMenu("Set A", "A swirl brownies set include Tahini swirl, Condensed milk and Jam.", new Brownie[] {
       new Brownie(1),
       new Brownie(2),
       new Brownie(3)
     }, 89),
-    new BrownieMenu("Set B", "A sprinkle brownies set include Peppermint patties, Ritz crackers and Oreos", new Brownie[] {
+    new BrownieMenu("Set B", "A sprinkle brownies set include Peppermint patties, Ritz crackers and Oreos.", new Brownie[] {
       new Brownie(4),
       new Brownie(5),
       new Brownie(6)
     }, 89),
-    new BrownieMenu("Set C", "A drop brownies set include Chocolate streusel, Peanut butter cookie dough and Black bottom", new Brownie[] {
+    new BrownieMenu("Set C", "A drop brownies set include Chocolate streusel, Peanut butter cookie dough and Black bottom.", new Brownie[] {
       new Brownie(7),
       new Brownie(8),
       new Brownie(9)
     }, 89),
-    new BrownieMenu("Set D", "A swoosh brownies set include Salted caramel, Matcha glaze and Nutella fudge", new Brownie[] {
+    new BrownieMenu("Set D", "A swoosh brownies set include Salted caramel, Matcha glaze and Nutella fudge.", new Brownie[] {
       new Brownie(10),
       new Brownie(11),
       new Brownie(12)
--- src/main/java/com/passawit/SmartShopApplication.java
+++ src/main/java/com/passawit/SmartShopApplication.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 @RestController
 public class SmartShopApplication {
+  private static ArrayList<BrownieMenu> cart = new ArrayList<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartShopApplication.class, args);
@@ -27,6 +30,30 @@ public class SmartShopApplication {
   @RequestMapping("/menu/{id}")
   BrownieMenu viewBrownieMenu(@PathVariable int id) {
     return BrownieMenuFactory.getBrownieMenu(id);
+  }
+
+  @RequestMapping("/menu/{id}/add")
+  String addToCart(@PathVariable int id) {
+	  cart.add(BrownieMenuFactory.getBrownieMenu(id));
+	  return String.format("\"%s\" has been added to the cart.", BrownieMenuFactory.getBrownieMenu(id).getName());
+  }
+
+  @RequestMapping("/cart")
+  String viewCart() {
+    double total = cart.stream().mapToDouble(BrownieMenu::getPrice).sum();
+    StringBuilder toString = new StringBuilder(String.format("Total number of items in cart is %d (", cart.size()));
+    for (BrownieMenu menu : cart) {
+      toString.append(String.format("\"%s\", ", menu.getName()));
+    }
+    toString.append(String.format(") Total: %.2f", total));
+    return toString.toString();
+  }
+
+  @RequestMapping("/checkout")
+  String checkout() {
+    double total = cart.stream().mapToDouble(BrownieMenu::getPrice).sum();
+    cart.clear();
+    return "Order has been received. Total price is " + total;
   }
 
 }
